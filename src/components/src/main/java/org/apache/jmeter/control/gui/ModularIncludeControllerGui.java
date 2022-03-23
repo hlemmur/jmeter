0a2919affd23e51b2d5c154d09b96f27a2cf73d6
Lena Mulyar <olena.mulyar@sebx.io>
2022-03-23 21:00:55 +0100

    WIP

--- src/components/src/main/java/org/apache/jmeter/control/gui/ModularIncludeControllerGui.java
+++ src/components/src/main/java/org/apache/jmeter/control/gui/ModularIncludeControllerGui.java
@@ -430,6 +430,14 @@ public class ModularIncludeControllerGui extends AbstractControllerGui implement
             GuiPackage.getInstance().updateCurrentGui();
         }
         if (e.getSource() == openIncludedTestPlanButton) {
+            /*
+            ProcessBuilder pb = new ProcessBuilder("myshellScript.sh", "myArg1", "myArg2");
+            Map<String, String> env = pb.environment();
+            env.put("VAR1", "myValue");
+            env.remove("OTHERVAR");
+            env.put("VAR2", env.get("VAR1") + "suffix");
+            pb.directory(new File("myDir"));
+            Process p = pb.start();*/
             org.apache.jmeter.NewDriver.main(new String[] {"-t", getAbsoluteFilePath(this.includePanel.getFilename())});
         }
 
